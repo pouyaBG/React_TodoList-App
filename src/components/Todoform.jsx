10d58ffd3505ead800b6c0d1030e086c3d96b519
Pouya Barari.G <b_pooya@yahoo.com>
2022-05-09 11:31:56 +0430

    component

---
+++ src/components/Todoform.jsx
@@ -0,0 +1,29 @@
+/** @format */
+
+import { useState } from "react";
+
+const TodoForm = (props) => {
+  const [input, setInput] = useState("");
+  const changeHandler = (event) => {
+    setInput(event.target.value);
+    console.log(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (!input) {
+      alert("please fill todo input");
+      return;
+    }
+    props.addTodoHandler(input);
+    setInput("");
+    // add todo user in list
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <input type="text" value={input} onChange={changeHandler}></input>
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default TodoForm;
